955345445f7c5c7bae7c9cf4e2799d5493014ff1
minni <mallavarapusudhishna@gmail.com>
2025-12-07 13:21:08 +0530

    Fix file path for writing tasks

--- app.py
+++ app.py
@@ -2,6 +2,6 @@
 
 #add task feature
 task = input("Enter a task: ")
-with open("src/tasks.txt", "a") as file:
+with open("tasks.txt", "a") as file:
     file.write(task + "\n")
 print("Task added!")
