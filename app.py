87351824902913e6d5c09475fac582724b236b33
minni <mallavarapusudhishna@gmail.com>
2025-12-11 13:59:21 +0530

    Revert "Add Author tag comment"
    This reverts commit bb775260aa5251d95b23a23cc75a5041a8ea2528.

--- app.py
+++ app.py
@@ -4,12 +4,6 @@
 print("Your tasks: ")
 with open("tasks.txt", "r") as file:
     tasks = file.readlines()
-<<<<<<< HEAD
 for t in tasks: 
     print("-"+t.strip())
 
-=======
-for t in tasks:
-    print("- "+t.strip())
-#Author: Sudhishna Mallavarapu
->>>>>>> a091027 (Add Author tag comment)
