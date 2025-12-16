9a71f1f539d320cbc1cb5f46c4006b32b670ca12
minni <mallavarapusudhishna@gmail.com>
2025-12-16 20:55:39 +0530

    changed app.py now.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Task Manager CLI App
 #Temporary change for stash practice.
+#Author: Sudhishna Mallavarapu
 #This program manages tasks using a CLI.
 print("Your tasks: ")
 with open("tasks.txt", "r") as file:
