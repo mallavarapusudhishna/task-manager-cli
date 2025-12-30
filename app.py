b5f51b3a24813b93fa47dc24bcb61b95b95c6d26
minni <mallavarapusudhishna@gmail.com>
2025-12-30 21:30:56 +0530

    app.py is modified

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Task Manager CLI App
 
 #This program manages tasks using a CLI 
-
+changes made her eare useless
 print("Your tasks: ")
 with open("tasks.txt", "r") as file:
     tasks = file.readlines()
